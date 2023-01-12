8985e2cab5ed49fae119d4fb87f353cfb5a8e551
Sean C Davis <scdavis41@gmail.com>
2023-01-11 21:11:08 -0500

    Simple broken link checker that reports broken links and pages checked

--- index.mjs
+++ index.mjs
@@ -1,11 +1,44 @@
 import { LinkChecker } from "linkinator";
-// import { URL } from "url";
-// import chalk from "chalk";
+import chalk from "chalk";
+import { URL } from "url";
+
+const BASE_URL = "https://www.seancdavis.com/";
+const LOGGER_MAP = {
+  OK: chalk.green("."),
+  BROKEN: chalk.red("!"),
+  SKIPPED: chalk.yellow("?"),
+};
 
 const checker = new LinkChecker();
 
+let brokenLinks = [];
 checker.on("link", (link) => {
-  console.log(link);
+  process.stdout.write(LOGGER_MAP[link.state]);
+  // Store reference if link is broken
+  if (link.state === "BROKEN") brokenLinks.push(link);
 });
 
-await checker.check({ recurse: true, path: "https://www.seancdavis.com/" });
+// Store reference to page being checked
+let pagesChecked = [];
+checker.on("pagestart", (url) => pagesChecked.push(url));
+
+await checker.check({ path: BASE_URL, recurse: true });
+
+// Report broken links
+if (brokenLinks.length > 0) {
+  console.log("\n");
+  console.log(`Found ${brokenLinks.length} broken links:`);
+  for (const brokenLink of brokenLinks) {
+    console.log("");
+    console.log(brokenLink.url);
+    console.log("  ", "STATUS:", brokenLink.status);
+    console.log("  ", "SOURCE:", new URL(brokenLink.parent).pathname);
+  }
+}
+
+// Report pages checked
+console.log("");
+console.log(`Checked ${pagesChecked.length} pages:`);
+for (const page of pagesChecked) {
+  console.log(" ", new URL(page).pathname);
+}
